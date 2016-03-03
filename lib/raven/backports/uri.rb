7377086b8ebda26592bbcd1c668bbaa96130d750
Krzysztof Lubomański <krzysztof.lubomanski@getresponse.com>
2016-03-03 15:10:03 +0100

    remove not needed methods

--- lib/raven/backports/uri.rb
+++ lib/raven/backports/uri.rb
@@ -7,36 +7,6 @@
 #
 
 module URI
-  # Encode given +s+ to URL-encoded form data.
-  #
-  # This method doesn't convert *, -, ., 0-9, A-Z, _, a-z, but does convert SP
-  # (ASCII space) to + and converts others to %XX.
-  #
-  # This is an implementation of
-  # http://www.w3.org/TR/html5/forms.html#url-encoded-form-data
-  #
-  # See URI.decode_www_form_component, URI.encode_www_form
-  def self.encode_www_form_component(s)
-    str = s.to_s
-    if RUBY_VERSION < "1.9" && $KCODE =~ /u/i
-      str.gsub(/([^ a-zA-Z0-9_.-]+)/) do
-        '%' + $1.unpack('H2' * Rack::Utils.bytesize($1)).join('%').upcase
-      end.tr(' ', '+')
-    else
-      str.gsub(/[^*\-.0-9A-Z_a-z]/) {|m| TBLENCWWWCOMP_[m]}
-    end
-  end
-
-  # Decode given +str+ of URL-encoded form data.
-  #
-  # This decodes + to SP.
-  #
-  # See URI.encode_www_form_component, URI.decode_www_form
-  def self.decode_www_form_component(str, enc = nil)
-    raise ArgumentError, "invalid %-encoding (#{str})" unless /\A(?:%[0-9a-fA-F]{2}|[^%])*\z/ =~ str
-    str.gsub(/\+|%[0-9a-fA-F]{2}/) {|m| TBLDECWWWCOMP_[m]}
-  end
-
   # Generate URL-encoded form data from given +enum+.
   #
   # This generates application/x-www-form-urlencoded data defined in HTML5
